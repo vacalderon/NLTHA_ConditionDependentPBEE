4f74d2df721917dcae48e9d578e42d21b60683e8
Victor Calderon <vacalder@ncsu.edu>
2022-02-18 13:19:48 -0500

    RESULTS FROM ANALYTICAL PROGRAM

--- DataPlotter/FragilityFunction/2022/1.0.1/220211/Sumary of CDF/main.py
+++ DataPlotter/FragilityFunction/2022/1.0.1/220211/Sumary of CDF/main.py
@@ -25,14 +25,15 @@ main_DF = pd.read_csv(data_file)
 
 number_of_plots_in_X = len(limit_states)
 number_of_plots_in_Y = len(axial_load_ratios)
-fig,axs = plt.subplots(number_of_plots_in_X, number_of_plots_in_Y)
-
+fig,axs = plt.subplots(number_of_plots_in_X, number_of_plots_in_Y,figsize=(10.0, 6.5),sharex=True,sharey=True, gridspec_kw=dict(wspace=0.30, hspace=0.30))
+plt.rcParams.update({'font.family':'serif'})
+plt.rcParams.update({'font.serif':'Times New Roman'})
 counter_x = -1
 counter_y = 0
 
 #Plot Controls
 
-linestyle_str = ['solid', 'dotted','dashed','dashdot','dashed']  # Same as '-.'
+linestyle_str = ['solid', 'dotted','dashed','dashdot',(0, (3, 5, 1, 5, 1, 5))]  # Same as '-.'
 
 linestyle_tuple = [
      ('solid',                 (0, ())), 
@@ -80,7 +81,28 @@ for i in limit_states:
             
             
             #plot CDF
-            axs[counter_x,counter_y].plot(x, y,color=plot_color,linestyle=lyne_type)
-            print('x=',counter_x)
-            print('y=',counter_y)
+            axs[counter_x,counter_y].plot(x, y,color=plot_color,linestyle=lyne_type)   
+            
+            if counter_x== 0:
+                axs[counter_x,counter_y].set_title("ALR="+str(j*100)+' %', fontsize=11)
+
+            if counter_x== 2:
+
+                axs[counter_x,counter_y].set_xlabel(r'Sd($t_{eff},\xi$) (in)')
+                
+            if counter_y== 0 and counter_x== 0:
+                axs[counter_x,counter_y].set_ylabel(r'Serviciability P[$\varepsilon>\varepsilon_{S}$]')
+            if counter_y== 0 and counter_x== 1:
+                axs[counter_x,counter_y].set_ylabel(r'Damage Control P[$\varepsilon>\varepsilon_{DC}$]')
+            if counter_y== 0 and counter_x== 2:
+                axs[counter_x,counter_y].set_ylabel(r'Ultimate P[$\varepsilon>\varepsilon_{u}$]')
+
+         
         counter_y+=1
+
+
+
+fig.legend(['CL = 0%','CL = 5%', 'CL = 10%','CL = 15%','CL = 20%'],loc="lower center",ncol=5,
+    bbox_to_anchor=(0.5,0.925),
+    bbox_transform=fig.transFigure,edgecolor='black' )
+plt.savefig("CDF_summary.pdf",dpi=600,bbox_inches='tight', pad_inches=0)
